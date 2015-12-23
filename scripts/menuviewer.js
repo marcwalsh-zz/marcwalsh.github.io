59aa684b79b635dc423f178dc4da3488018e67c6
marcwalsh <marc@marcwalsh.co.uk>
2015-12-23 01:51:58 +0000

    getting there

--- scripts/menuviewer.js
+++ scripts/menuviewer.js
@@ -1,12 +1,14 @@
-var aboutnav = document.querySelector ('#aboutnav');
-var ourwork = document.querySelector ('#ourwork');
-var services = document.querySelector ('#services');
-var theteam = document.querySelector ('#theteam');
-var contact = document.querySelector ('#contact');
-
-document.querySelector('aboutnav').addEventListener('click', function() {
-        ourwork.setAttribute('src', '/images/about2.png');
-        services.setAttribute('src', '/images/about3.png');
-        theteam.setAttribute('src', '/images/about4.png');
-        contact.setAttribute('src', '/images/about5.png');
-    });
+var aboutnav = document.querySelector('#aboutnav');
+var ourwork = document.querySelector('#ourwork');
+var services = document.querySelector('#services');
+var theteam = document.querySelector('#theteam');
+var contact = document.querySelector('#contact');
+var reset = document.querySelector('#reset');
+    
+document.querySelector('#aboutnav').addEventListener('click', function() {
+    ourwork.setAttribute('src', '/images/about2.png');
+    services.setAttribute('src', '/images/about3.png');
+    theteam.setAttribute('src', '/images/about4.png');
+    contact.setAttribute('src', '/images/about5.png');
+    
+});
